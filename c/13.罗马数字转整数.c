c26e506bcac9785bf189ab0ccf86c0645d872d1c
duckoutain <duckoutain@gmail.com>
2020-03-17 21:16:47 +0800

    增加１３．罗马数字转整数C语言解法

---
+++ c/13.罗马数字转整数.c
@@ -0,0 +1,18 @@
+int romanToInt(char * s){
+	int count = 0;
+	while (*s){
+		if (*s == 'V')         count += 5;
+		else if (*s == 'L')    count += 50;
+		else if (*s == 'D')    count += 500;
+		else if (*s == 'M')    count += 1000;
+		else if (*s == 'I')
+			count = (*(s + 1) == 'V' || *(s + 1) == 'X') ? count - 1 : count + 1;
+		else if (*s == 'X')
+			count = (*(s + 1) == 'L' || *(s + 1) == 'C') ? count - 10 : count + 10;
+		else
+			count = (*(s + 1) == 'D' || *(s + 1) == 'M') ? count - 100 : count + 100;
+		s++;
+	}
+	return count;
+
+}
